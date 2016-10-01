e54796ef0e1d582a60db3771a1687e1bb7c12978
doome1337 <dmitry.paramonov@gmail.com>
2016-10-01 14:12:02 -0400

    Barebones AI Version 0.3.
    This AI can pick targets more intelligently.

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -1,6 +1,7 @@
 import com.orbischallenge.ctz.Constants;
 import com.orbischallenge.game.engine.*;
 import com.orbischallenge.ctz.objects.*;
+import com.orbischallenge.ctz.objects.enums.*;
 
 
 public class PlayerAI {
@@ -8,21 +9,57 @@ public class PlayerAI {
     public PlayerAI() {
     }
 
-    private ControlPoint findNearestMainframe(World world, Point p) {
+    private int enemyNumber(Team ours, Team other) {
+        if (other == Team.NONE) {
+            return 0;
+        }
+        if (ours == Team.BLUE && other == Team.BLUE) {
+            return 1;
+        }
+        if (ours == Team.AMBER && other == Team.AMBER) {
+            return 1;
+        }
+        return -1;
+    }
+
+    private ControlPoint findPriorityControlPoint(
+            World world,
+            Point p,
+            Team friendly,
+            double mainframeMultiplierCost,
+            double friendlyMultiplierCost,
+            double enemyMultiplierCost
+            ) {
+        /**
+         * Breaks if no control points on map.
+         */
         ControlPoint[] allPoints = world.getControlPoints();
         ControlPoint closest = null;
-        int longest = 1000;
-        for (ControlPoint cp: allPoints) {
-            if (cp.isMainframe()) {
-                int len = world.getPathLength(p, cp.getPosition());
-                if (len < longest) {
-                    longest = len;
-                    closest = cp;
-                }
-            }
+        double longest = Double.NaN;
+        if (mainframeMultiplierCost != mainframeMultiplierCost) {
+            mainframeMultiplierCost = 1.0;
         }
-        if (closest == null) {
-            closest = world.getNearestControlPoint(p);
+        if (friendlyMultiplierCost != friendlyMultiplierCost) {
+            friendlyMultiplierCost = 1.0;
+        }
+        if (enemyMultiplierCost != enemyMultiplierCost) {
+            enemyMultiplierCost = 1.0;
+        }
+        for (ControlPoint cp: allPoints) {
+            double len = world.getPathLength(p, cp.getPosition());
+            if (cp.isMainframe()) {
+                len *= mainframeMultiplierCost;
+            }
+            if (enemyNumber(friendly, cp.getControllingTeam()) == -1) {
+                len *= enemyMultiplierCost;
+            }
+            if (enemyNumber(friendly, cp.getControllingTeam()) == 1) {
+                len *= friendlyMultiplierCost;
+            }
+            if (longest != longest || len < longest) {
+                longest = len;
+                closest = cp;
+            }
         }
         return closest;
     }
@@ -31,12 +68,25 @@ public class PlayerAI {
 	 * This method will get called every turn.
 	 *
 	 * @param world The latest state of the world.
-	 * @param enemyUnits An array of all 4 units on the enemy team. Their order won't change.
-	 * @param friendlyUnits An array of all 4 units on your team. Their order won't change.
+	 * @param enemyUnits An array of all 4 units on the enemy team.
+     *  Their order won't change.
+	 * @param friendlyUnits An array of all 4 units on your team.
+     *  Their order won't change.
 	 */
-    public void doMove(World world, EnemyUnit[] enemyUnits, FriendlyUnit[] friendlyUnits) {
+    public void doMove(
+            World world,
+            EnemyUnit[] enemyUnits,
+            FriendlyUnit[] friendlyUnits) {
         for (FriendlyUnit f: friendlyUnits) {
-            f.move(world.getNextDirectionInPath(f.getPosition(),findNearestMainframe(world, f.getPosition()).getPosition()));
+            f.move(world.getNextDirectionInPath(
+                f.getPosition(),
+                findPriorityControlPoint(
+                    world,
+                    f.getPosition(),
+                    f.getTeam(),
+                    0.3,
+                    5,
+                    0.5).getPosition()));
         }
     }
 }
